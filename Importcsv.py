6a617e0c980c7bf91b10e81b2b527d1c510a9196
Your Name <you@example.com>
2021-04-28 17:44:26 +0200

    master

--- Importcsv.py
+++ Importcsv.py
@@ -1,10 +1,12 @@
-import csv
+import csv as csv
 
 # Dossier où se trouve le fichier :
-folder = ''
-filename = 'covid-hospit-incid-reg-2021-03-03-17h20.csv'
+folder = "C:/Users/jules/Desktop/POO/PPOO/Donnees/Données/"
+filename = "covid-hospit-incid-reg-2021-03-03-17h20.csv"
 data = []
-with open(folder + filename, encoding='ISO-8859-1') as csvfile :
-covidreader = csv.reader(csvfile, delimiter=' ;')
-for row in covidreader :
-    data.append(row)
+
+with open(folder + filename, encoding ='ISO-8859-1') as csvfile :
+    covidreader = csv.reader(csvfile, delimiter=';')
+    for row in covidreader :
+        data.append(row)
+
