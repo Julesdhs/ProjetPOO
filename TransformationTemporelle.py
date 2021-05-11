308abb1b3fafdb2d274ffbefaed98efef5a5bf8a
lk34000 <83410683+lk34000@users.noreply.github.com>
2021-05-11 18:20:22 +0200

    Add files via upload

--- TransformationTemporelle.py
+++ TransformationTemporelle.py
@@ -1,26 +1,32 @@
-from TransformationAsbtraite import TransformationAbstraite
 
-class TransformationTemporelle(TransformationAsbtraite):
+class TransformationTemporelle(TransformationAbstraite):
     def __init__(self,debut_periode,fin_periode):
         self.debut_periode = debut_periode
         self.fin_periode = fin_periode
 
-
     def transform(self,table):
 
-        '''les données sont classées par ordre temporel et la date est un jour, noté par la variable jour dans les tables, on considère des inégalités larges dans debut_periode et fin_periode. '''
+        '''les données sont classées par ordre temporel et la date est un jour, noté par la variable jour dans les tables, on considère des inégalités larges dans debut_periode et strictes pour fin_periode
+        Si les données ne sont pas classées par odre temporel il faudra utiliser le module date time et trier les données au préalable : ce n'est pas notre cas ici'''
 
-        ind = table.colonnes.index(jour)
+        ind = table.colonnes.index('jour')
         n=len(table.contenu)
-        for l in range(n) :
+        newt = Table()
+        l = 0
+        x = table.contenu[l][ind]
+        if x== self.debut_periode:
+            while x!= self.fin_periode:
+                newt.ajoutlig(table.contenu[l])
+                l+=1
+                x = table.contenu[l][ind]
+        else:
+            l+=1
             x = table.contenu[l][ind]
-            while x != self.debut_periode :
-                table.enlevlig(l)
-            if x == self.fin_periode:
-                indfin = l
+        table.contenu = newt.contenu
 
-        for i in range(indfin + 1,l):
-            table.enlevelig(i)
+
+
+
 
 
 
