640bc0ae858fb2ee8d3d301b0d9128398ceade80
Julesdhs <83074363+Julesdhs@users.noreply.github.com>
2021-05-12 01:37:48 +0200

    Add files via upload

--- Pipeline.py
+++ Pipeline.py
@@ -1,11 +1,21 @@
-class Pipeline():
-    def __init__(self):
-        self.operations=[]
+#from OperationAbstraite import OperationAbstraite
 
-    def ajoute_etape(self,etape):
-        self.operations.append(etape)
-        
-    def run(self,table):
-        for etape in self.operations:
-            table=etape.process(table)
-        return(table)
+class Pipeline():
+    def __init__(self,table,pile= None) :
+        self.table = table
+        if pile is None:
+            pile = []
+        self.pile = pile
+
+    def ajout_etape(self,operation):
+        self.pile.append(operation)
+
+    def applique(self):
+        res=[]
+        j=0
+        for k in range(len(self.pile)):
+            if len(self.pile) != 0 :
+                res.append(self.pile[k-j].process(self.table))
+                del self.pile[0]
+                j+=1
+        return(res)
