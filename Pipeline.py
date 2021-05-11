24ea283847160a12c3a721142ceced9a708abe45
Julesdhs <83074363+Julesdhs@users.noreply.github.com>
2021-05-11 17:05:28 +0200

    Add files via upload

--- Pipeline.py
+++ Pipeline.py
@@ -15,9 +15,11 @@ class Pipeline():
         self.pile.append(operation)
 
     def applique(self):
+        res=[]
         j=0
         for k in range(len(self.pile)):
             if len(self.pile) != 0 :
-                self.pile[k-j].process(self.table)
+                res.append(self.pile[k-j].process(self.table))
                 del self.pile[0]
                 j+=1
+        return(res)
