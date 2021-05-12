47006427f3b843cb148bedb1b4044ab54c79403b
Emy-O <83076185+Emy-O@users.noreply.github.com>
2021-05-12 14:33:52 +0200

    Update EstimateurVariance.py

--- EstimateurVariance.py
+++ EstimateurVariance.py
@@ -1,22 +1,20 @@
-#from EstimateurAbstraite import EstimateurAbstraite
+from EstimateurAbstraite import EstimateurAbstraite
 
 class EstimateurVariance(EstimateurAbstraite):
-
     def __init__(self,nom_col):
-        self.nom_col = nom_col
-
+        self.nom_col=nom_col
+    
     @staticmethod
-    def variance(table,nom_col):
+    def variance (table,nom_col):
         indice=table.colonnes.index(nom_col)
         nb_obs=len(table.contenu)
-        moy=EstimateurMoyenne.moyenne(table,nom_col)
+        moy=table.moyenne()
         var=0
-        for ligne in range(nb_obs):
+        for ligne in range nb_obs:
             var+=table.contenu[ligne][indice]**2
-        return(var/nb_obs-moy**2)
+        return(var/nb_obs-moy**2) 
 
-
-    def fit(table):
-        table_resultat=Table()
-        table_resultat.ajoutcol("variance de" + nom_col, [EstimateurMoyenne.moyenne(table,nom_col)])
+    
+    def fit(self,table):
+        table_resultat=Table(["variance de" + nom_col], [variance(table,self.nomcol)])
         return(table_resultat)
