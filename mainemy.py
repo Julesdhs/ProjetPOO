d6a061882723533ad7d09f824bca5ddebbdbc910
lk34000 <83410683+lk34000@users.noreply.github.com>
2021-05-12 19:15:47 +0200

    Update mainemy.py

--- mainemy.py
+++ mainemy.py
@@ -1,4 +1,4 @@
-from Importcsv import Import 
+from Importcsv import Import
 from EstimateurSomme import EstimateurSomme
 from Pipeline import Pipeline
 folder = "P:/PTD/Donnees/Données/"
@@ -14,10 +14,50 @@ a = EstimateurSomme('hosp')
 EstimateurSomme.somme(t,'hosp')
 res=a.fit(t)
 
-folder = "P:/PTD/Donnees/Données/"
+
+## Question 1): Quel est le nombre total d'hospitalisation dues au covid-19?
+#on crée la table
+folder = "C:/Users/leonk/Documents/ProjetPOO-master/Donnees/Données/"
 filename = "donnees-hospitalieres-nouveaux-covid19-2021-03-03-17h03.csv"
-table=Import.cree(folder,filename)
-Question1=Pipeline()
-Question1.ajoute_etape(EstimateurSomme('incid_hosp'))
-res=Question1.run(table)
-print(res.contenu)
+table=Import.creecsv(folder,filename)
+table.stringtoint('incid_hosp')
+table.stringtoint('incid_rea')
+table.stringtoint('incid_dc')
+table.stringtoint('incid_rad')
+#on crée la pipeline
+Question1=Pipeline(table)
+Question1.ajout_etape(EstimateurSomme('incid_hosp'))
+res1=Question1.applique()
+#on retourne le résultat
+print(res[0].contenu)
+
+## Question 2) Combien de nouvelles hospitalisations ont eu lieu ces 7 derniers jours dans chaque département?
+#on crée la table
+folder = "C:/Users/leonk/Documents/ProjetPOO-master/Donnees/Données/"
+filename = "donnees-hospitalieres-nouveaux-covid19-2021-03-03-17h03.csv"
+table=Import.creecsv(folder,filename)
+table.stringtoint('incid_hosp')
+table.stringtoint('incid_rea')
+table.stringtoint('incid_dc')
+table.stringtoint('incid_rad')
+#on crée la pipeline
+today = '2021-03-03'
+Question2=Pipeline(table)
+Question2.ajout_etape(TransformationTemporelle('2021-02-25','2021-03-03'))
+Question2.applique()
+#on fait des modifications
+tabledep=Import.creecsv(folder,filename)
+TransformationSelectionVariables(['dep']).transform(tabledep)
+tableres = Table(['dep'],tabledep.contenu[0:101])
+indhosp = table.colonnes.index('incid_hosp')
+inddep = table.colonnes.index('dep')
+col = [0 for k in range(101)]
+for ligne in table.contenu:
+    indice = tableres.contenu.index([ligne[inddep]])
+    col[indice]+= ligne[indhosp]
+tableres.ajoutcol('nouv_hosp_hebdo',col)
+        
+
+
+##Question 3) Comment évolue la moyenne des nouvelles hospitalisations journalières de cette semaine par rapport à celle de la semaine dernière?
+
