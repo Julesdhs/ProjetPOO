814c8d1644bc599c210704d633cbabd281cd6ab9
Emy-O <83076185+Emy-O@users.noreply.github.com>
2021-05-12 15:18:39 +0200

    Add files via upload

---
+++ Export.py
@@ -0,0 +1,17 @@
+from csv import csv 
+
+class Export():
+    def __init__(self,nom):
+        self.nom=nom
+        
+
+    def fit(self,table):
+        with open(self.nom, 'w') as f:
+
+            write = csv.writer(f, delimiter = ',')
+            write.writerow(table.colonnes)
+            write.writerows(table.contenu)
+            
+            
+
+            
