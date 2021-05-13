c59252ce1341234a04465802b97fe688490b2d6b
lk34000 <83410683+lk34000@users.noreply.github.com>
2021-05-13 13:16:23 +0200

    Update TransformationTemporelle.py

--- TransformationTemporelle.py
+++ TransformationTemporelle.py
@@ -10,15 +10,22 @@ class TransformationTemporelle(TransformationAbstraite):
         '''les données sont classées par ordre temporel et la date est un jour, noté par la variable jour dans les tables, on considère des inégalités larges dans debut_periode et strictes pour fin_periode
         Si les données ne sont pas classées par odre temporel il faudra utiliser le module date time et trier les données au préalable : ce n'est pas notre cas ici'''
 
-
-
-        newtable=Table()
-        ind = table.colonnes.index('jour')
-        for ligne in table.contenu:
-            if ligne[ind]>= self.debut_periode and ligne[ind]<= self.fin_periode:
-                newtable.ajoutlig(ligne)
-        table.contenu=newtable.contenu
-            
+        if 'jour' in table.colonnes:
+            newtable=Table()
+            ind = table.colonnes.index('jour')
+            for ligne in table.contenu:
+                if ligne[ind]>= self.debut_periode and ligne[ind]<= self.fin_periode:
+                    newtable.ajoutlig(ligne)
+            table.contenu=newtable.contenu
+            table.enlevlig(1)
+        else:
+            newtable=Table()
+            ind = table.colonnes.index('debut')
+            for ligne in table.contenu:
+                if ligne[ind]>= self.debut_periode and ligne[ind]<= self.fin_periode:
+                    newtable.ajoutlig(ligne)
+            table.contenu=newtable.contenu
+            table.enlevlig(1)   
 
 
 
