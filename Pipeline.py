b6c419d3315df6f61303bbb26c20df2c10a5931f
Julesdhs <83074363+Julesdhs@users.noreply.github.com>
2021-05-13 20:07:56 +0200

    Update Pipeline.py

--- Pipeline.py
+++ Pipeline.py
@@ -1,24 +1,22 @@
 #from OperationAbstraite import OperationAbstraite
 
 class Pipeline():
-    def __init__(self,table,pile= None) :
+    def __init__(self,table,pile = None,res = None) :
         self.table = table
         if pile is None:
             pile = []
+        if res is None :
+            res = []
         self.pile = pile
+        self.res = res
 
     def ajout_etape(self,operation):
         self.pile.append(operation)
 
     def applique(self):
-        res=[]
-        j=0
         for k in range(len(self.pile)):
-            if len(self.pile) != 0 :
-                res.append(self.pile[k-j].process(self.table))
-                del self.pile[0]
-                j+=1
-        return(res)
+                self.res.append(self.pile[k].process(self.table))
+        return(self.res)
 
         ##
 class Pipeline():
