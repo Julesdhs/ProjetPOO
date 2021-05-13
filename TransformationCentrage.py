f2e7242e005d13b30563315689547d50a602fbdc
Emy-O <83076185+Emy-O@users.noreply.github.com>
2021-05-13 22:04:39 +0200

    Update TransformationCentrage.py

--- TransformationCentrage.py
+++ TransformationCentrage.py
@@ -1,17 +1,39 @@
-#from TransformationAsbtraite import TransformationAbstraite
+from TransformationAsbtraite import TransformationAbstraite
 
 class TransformationCentrage(TransformationAbstraite):
-
+    '''la classe permet de centrer un certain nombre de colonnes d'une table.
+    
+    Attribus
+    ----------
+    listcol: list[str]
+        liste des nom de colonnes de la table à centrer
+    '''
     def __init__(self,listcol):
+        """ Constructeur de la classe TransformationCentrage
+
+        Paramètres
+        ----------
+        listcol: list[str]
+            liste des nom de colonnes de la table à centrer
+        """   
         self.listcol = listcol
 
     def transform(self,table):
+        """ Méthode transform de la classe TransformationCentrage 
+    
+        Cette méthode permet d'effectuer la centralisation d'une liste de variables de la table.
+        Elle utilise la méthode transform de la TranformationCentrage et la variance de l'EstimateurMoyenne.
+        
+        Paramètres
+        ----------
+        table : Table
+            table sur laquelle on veut effectuer la centralisation
+        """
         for nomcol in self.listcol:
             i=table.colonnes.index(nomcol)
             moy = EstimateurMoyenne.moyenne(table,table.colonnes[i])
             for j in range(len(table.contenu)) :
                 table.contenu[j][i] = table.contenu[j][i] - moy
 
-#testé 05/05 14h
 
 
