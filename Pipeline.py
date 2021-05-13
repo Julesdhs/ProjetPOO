0d2d1423aafeb89090a708e0f7977664b2862d0c
lk34000 <83410683+lk34000@users.noreply.github.com>
2021-05-13 20:10:39 +0200

    Update Pipeline.py

--- Pipeline.py
+++ Pipeline.py
@@ -1,3 +1,4 @@
+
 #from OperationAbstraite import OperationAbstraite
 
 class Pipeline():
@@ -17,35 +18,3 @@ class Pipeline():
         for k in range(len(self.pile)):
                 self.res.append(self.pile[k].process(self.table))
         return(self.res)
-
-        ##
-class Pipeline():
-    def __init__(self):
-        self.operations=[]
-
-    def ajout_etape(self,etape):
-        self.operations.append(etape)
-
-    def applique(self,table):
-        res=[]
-        for etape in self.operations:
-            res.append(etape.process(table))
-        return(res)
-
-
-
-folder = "P:/PTD/Donnees/Données/"
-filename = "donnees-hospitalieres-nouveaux-covid19-2021-03-03-17h03.csv"
-table=Import.creecsv(folder,filename)
-table.stringtoint('incid_hosp')
-
-# Semaine1
-
-debut_semaine1='2021-02-18'
-fin_semaine1='2021-02-24'
-
-pip2=Pipeline()
-pip2.ajout_etape(TransformationTemporelle(debut_semaine1,fin_semaine1))
-pip2.ajout_etape(EstimateurMoyenne('incid_hosp'))
-res2=pip2.applique(table)
-res2[-1].contenu
